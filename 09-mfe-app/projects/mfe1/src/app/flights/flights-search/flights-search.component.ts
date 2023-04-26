5b43985bb875b48ad7418d07d5f3e283661e7e2a
Magesh Kuppan <tkmagesh77@gmail.com>
2023-04-26 16:53:35 +0530

    MFE demo

--- 09-mfe-app/projects/mfe1/src/app/flights/flights-search/flights-search.component.ts
+++ 09-mfe-app/projects/mfe1/src/app/flights/flights-search/flights-search.component.ts
@@ -1,13 +1,21 @@
 import {Component} from '@angular/core';
 import { AuthLibService } from 'auth-lib';
-
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-flights-search',
   templateUrl: './flights-search.component.html'
 })
 export class FlightsSearchComponent {
-  constructor(private service: AuthLibService) {
+
+  constructor(private service: AuthLibService, private httpClient : HttpClient) {
+    
+   
     console.log('User Name', this.service.user);
+  }
+
+  onBtnGetDataClick() {
+    this.httpClient.get('http://localhost:3500/bugs')
+      .subscribe(data => console.table(data))
   }
 }
---
+++ wk-wfe-app/projects/shell/webpack.config.js
@@ -0,0 +1,14 @@
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+
+  remotes: {
+    "mfe1": "http://localhost:3200/remoteEntry.js",
+    "mfe2": "http://localhost:4200/remoteEntry.js",    
+  },
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+
+});
